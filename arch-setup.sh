c219e57228d95d4880706a64becae67f3886baf3
h-youhei <hukumitu.youhei@gmail.com>
2016-11-25 17:49:55 +0900

    fix locale.gen

--- arch-setup.sh
+++ arch-setup.sh
@@ -5,7 +5,7 @@ script_dir=`dirname $script_path`
 
 ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 hwclock --systohc
-printf "en_US.UTF-8\nja_JP.UTF-8\n" >> /etc/locale.gen
+printf "en_US.UTF-8 UTF-8\nja_JP.UTF-8 UTF-8\n" >> /etc/locale.gen
 locale-gen
 
 $script_dir/boot-setup.sh
